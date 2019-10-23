f349559813b97b33b360c276e9b23c4083baeb7f
viktor <kruvv@mail.ru>
2019-10-23 12:57:27 +0300

    Add filtering Entities in a Grid

--- my-vaadin-webapp/src/main/java/ru/kruvv/vaadin/app/spring/MainView.java
+++ my-vaadin-webapp/src/main/java/ru/kruvv/vaadin/app/spring/MainView.java
@@ -2,6 +2,8 @@ package ru.kruvv.vaadin.app.spring;
 
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.PWA;
 
@@ -11,11 +13,21 @@ public class MainView extends VerticalLayout {
 
 	private CustomerService service = CustomerService.getInstance();
 	private Grid<Customer> grid = new Grid<>(Customer.class);
+	private TextField filterText = new TextField();
 
 	public MainView() {
+
+		// Adding a TextField Component
+		filterText.setPlaceholder("Filter by name...");
+		filterText.setClearButtonVisible(true);
+
+		// Filtering the Data
+		filterText.setValueChangeMode(ValueChangeMode.EAGER);
+		filterText.addValueChangeListener(e -> updateList());
+
 		grid.setColumns("firstName", "lastName", "status");
 
-		add(grid);
+		add(filterText, grid);
 
 		setSizeFull();
 
@@ -23,7 +35,7 @@ public class MainView extends VerticalLayout {
 	}
 
 	public void updateList() {
-		grid.setItems(service.findAll());
+		grid.setItems(service.findAll(filterText.getValue()));
 	}
 
 }
